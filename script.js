3a10e179da2646f2c2abf80796c88e86999ac690
Devanshu Shyamsundar <132264393+devanshu-0534@users.noreply.github.com>
2026-01-09 12:29:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+
+codes[0].focus();
+
+codes.forEach((code, index) => {
+  code.addEventListener('keydown', (e) => {
+
+    // Allow only numbers and backspace
+    if (
+      e.key >= '0' && e.key <= '9'
+    ) {
+      code.value = '';
+      setTimeout(() => {
+        if (index < codes.length - 1) {
+          codes[index + 1].focus();
+        }
+      }, 10);
+    }
+
+    else if (e.key === 'Backspace') {
+      code.value = '';
+      if (index > 0) {
+        setTimeout(() => {
+          codes[index - 1].focus();
+        }, 10);
+      }
+    }
+
+    else {
+      e.preventDefault();
+    }
+  });
+});
